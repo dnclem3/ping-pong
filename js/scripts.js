3c13d7539c9bbf9aad9a43494dcf498b48278250
Dan Clem <daniel.p.clem@gmail.com>
2017-03-09 20:58:19 -0800

    style mirrors that of the website

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@ $(document).ready(function() {
   $("form").submit(function(e) {
     e.preventDefault();
     var input = $("#entry").val();
-    for (i = 1; input > i; i++) {
+    for (i = 1; input > i - 1; i++) {
       if (i % 15 == 0) {
-        $("#output").append("pingpong");
+        $("#output").append("<li>pingpong</li>");
       }
       else if (i % 3 == 0) {
-        $("#output").append("ping");
+        $("#output").append("<li>ping</li>");
       }
       else if (i % 5 == 0) {
-        $("#output").append("pong");
+        $("#output").append("<li>pong</li>");
       }
       else {
-        $("#output").append(i);
-        console.log(i);
+        $("#output").append("<li>" + i + "</li>");
       }
     };
   });
